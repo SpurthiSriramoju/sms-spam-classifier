a365ed940c3ed9d083dbd917d6d76030ae5fe68b
SpurthiSriramoju <154662050+SpurthiSriramoju@users.noreply.github.com>
2024-07-04 12:24:51 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='sms-spam-classifier',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='HP',
+    author_email='',
+    description=''
+)
